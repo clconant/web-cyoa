df0af84d2839551a3904d958eeb28e6fd2534c39
clconant <42870762+clconant@users.noreply.github.com>
2018-11-15 11:46:48 -0500

    Add files via upload

---
+++ theman.js
@@ -0,0 +1,63 @@
+/* This is a starter with examples.  Remove the examples once you have made your own versions. */
+
+'use strict'
+
+// core variables
+var messages = []; // for a sequence of messages
+var choices = [];
+var answer = null;
+
+// story function variables
+var name = null;
+var spin = 0;
+
+function checkAnswers(answer) {  // Function names match story scenes. Replace with your functions
+     if (answer == "Walk away") {
+        cornerOfTheBuilding();
+     } 
+      else if (answer == "Walk to him") {
+        spinInCircle();
+     }
+}
+
+function theMan(){
+	story("You are wandering along a sidewalk at midnight in near pitchblack with only streetlights illuminating the path that you have chosen. You have plenty of time that you would want to waste but out of the corner of your eye you see a shadown of a man leaning against a building asking you to go talk with him. What do you want to do?")
+	choices = ["Walk Away", "Walk to him", "Call the police"];
+	answer = setOptions(choices);
+}
+
+function cornerOfTheBuilding(){
+	story("You decide to go talk with the creepy guy that told you to come over to him at midnight while you were walking, he stares you down as you slowly approach towards him and escape the streetlights illumination")
+	choices = ["Go home", "Run into the forest", "Run into the building"];
+	answer = setOptions(choices);
+}
+
+function wildNightmares(){
+	story("You chose to talk with the guy HOW WISE you guys have a good 30 minute conversation about your favorite sports teams and the sports you guys like the best")
+	choices = ["Enter now", "Tell mom"];
+	answer = setOptions(choices);
+}
+
+function walkingToTheBack(){
+	story("The guy starts walking away and you follow him around to the back of the building where there's no cameras or light")
+	choices = ["Run away", "Fight Him", "Call the police"];
+	answer = setOptions(choices);
+}
+
+function gettingDraggedAway(){
+	story("")
+	choices = ["","","",];
+	answer = setOptions(choices);
+
+
+/* function forest() {  // HOW TO SET A SCENE
+ * story("There is a house made of candy\
+ * \nThere is a fruit vender\
+ * \nThere is a small opening in the trees with a ring of mushroom in the middle");
+ * 
+ * choices = ["Candy house", "Fruit vender", "Field", "Hang out here", "spin in a circle"];
+ * answer = setOptions(choices);
+ *    
+ * }
+ */ 
+ 
